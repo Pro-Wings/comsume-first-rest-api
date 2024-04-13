160328666fb112e3b9c08a95a0c970783c18f853
JD <jayprowings@gmail.com>
2024-04-13 11:23:43 +0530

    first draft for consuming rest api from another rest api

---
+++ src/main/java/com/prowings/myapp/service/RestApiConsumeService.java
@@ -0,0 +1,14 @@
+package com.prowings.myapp.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.prowings.myapp.model.Student;
+
+public interface RestApiConsumeService {
+	
+	public ResponseEntity<List> getStds();
+	
+
+}
---
+++ src/main/java/com/prowings/myapp/service/RestApiConsumeServiceImpl.java
@@ -0,0 +1,37 @@
+package com.prowings.myapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.prowings.myapp.model.Student;
+
+@Service
+public class RestApiConsumeServiceImpl implements RestApiConsumeService {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Override
+	public ResponseEntity<List> getStds() {
+
+		System.out.println(">>>> student service :: getStd() started!!");
+		// call to rest api
+
+		String uri = "http://localhost:8084/my-first-student-rest-api/students";
+
+//		Student fetchedStd = restTemplate.getForObject(uri, Student.class);
+
+		ResponseEntity<List>  fetchedStd = restTemplate.getForEntity(uri, List.class);
+
+		System.out.println(">>> received Std from REST Api : " );
+		System.out.println(">>> Response Body : "+ fetchedStd.getBody());
+		System.out.println(">>> Response Status : "+ fetchedStd.getStatusCodeValue());
+
+		return fetchedStd;
+	}
+
+}
